228b8d13fdad86180442280f7065b876ac827344
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2016-01-12 10:28:17 +0100

    Make SlaveID getter private and link to addSlave

--- java/com/sfh/dsb/SlaveID.java
+++ java/com/sfh/dsb/SlaveID.java
@@ -3,12 +3,14 @@ package com.sfh.dsb;
 
 /**
  * An opaque handle to a slave in an execution.
+ *
+ * @see ExecutionController#addSlave
  */
 public class SlaveID
 {
     SlaveID(int id) { this.id = id; }
 
-    public int getID() { return id; }
+    int getID() { return id; }
 
     private int id = -1;
 }
