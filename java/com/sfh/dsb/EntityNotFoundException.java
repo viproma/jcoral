5de0022fe2445cd7a01b1da04cfc3401f4352a04
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2016-05-25 10:02:59 +0200

    Fix some small issues with exception classes
    - Wrong doc comment format.
    - Missing serialVersionUID.

--- java/com/sfh/dsb/EntityNotFoundException.java
+++ java/com/sfh/dsb/EntityNotFoundException.java
@@ -7,10 +7,14 @@ package com.sfh.dsb;
  */
 public class EntityNotFoundException extends Exception
 {
-    /// Constructs an exception with the given message.
+    /** Constructs an exception with the given message. */
     public EntityNotFoundException(String message)
     {
         super(message);
     }
+
+    // Strongly recommended for Serializable classes, see:
+    // https://docs.oracle.com/javase/7/docs/api/java/io/Serializable.html
+    private static final long serialVersionUID = 0L;
 }
 
--- java/com/sfh/dsb/ModelConstructionException.java
+++ java/com/sfh/dsb/ModelConstructionException.java
@@ -1,12 +1,16 @@
 package com.sfh.dsb;
 
 
-/// Exception type thrown on model construction errors.
+/** Exception type thrown on model construction errors. */
 public class ModelConstructionException extends Exception
 {
-    /// Constructs an exception with the given message.
+    /** Constructs an exception with the given message. */
     public ModelConstructionException(String message)
     {
         super(message);
     }
+
+    // Strongly recommended for Serializable classes, see:
+    // https://docs.oracle.com/javase/7/docs/api/java/io/Serializable.html
+    private static final long serialVersionUID = 0L;
 }
